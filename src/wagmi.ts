2bfec5fbec20e42ffc11dbf0150f2771cbef954f
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-09-22 20:40:33 -0700

    Temporary wagmi config: will break on mainnet

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,8 @@
+import "dotenv/config";
 import { configureChains, createConfig } from "wagmi";
 import { goerli, mainnet } from "wagmi/chains";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-
 import { publicProvider } from "wagmi/providers/public";
-
-const walletConnectProjectId = "130c92c32b5cfbac34b8cff6780340e7";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, ...(process.env.NODE_ENV === "development" ? [goerli] : [])],
@@ -17,7 +15,7 @@ export const config = createConfig({
     new WalletConnectConnector({
       chains,
       options: {
-        projectId: walletConnectProjectId,
+        projectId: "130c92c32b5cfbac34b8cff6780340e7",
       },
     }),
   ],
