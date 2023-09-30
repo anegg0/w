dfe60b11bdf0e803c5859bbafcb5852885721860
Gaël Blanchemain <anegg0@users.noreply.github.com>
2023-09-30 01:52:42 +0000

    added node.env to wagniconfig

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -5,28 +5,28 @@ import { mainnet, sepolia } from "wagmi/chains";
 import * as chains from "wagmi/chains";
 
 export default defineConfig({
-  out: "src/generated.ts",
-  contracts: [],
-  env: [
-  loadEnv({
-    mode: process.env.NODE_ENV,
-    envDir: process.cwd(),
-  })
-],
-  plugins: [
-    etherscan({
-      apiKey: process.env.ETHERSCAN_API_KEY!,
-      chainId: mainnet.id,
-      contracts: [
-        {
-          name: "WagmiMintExample",
-          address: {
-            [chains.mainnet.id]: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
-            [chains.goerli.id]: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
-          },
-        },
-      ],
-    }),
-    react(),
-  ],
+	out: "src/generated.ts",
+	contracts: [],
+	env: [
+		loadEnv({
+			mode: process.env.NODE_ENV,
+			envDir: process.cwd(),
+		}),
+	],
+	plugins: [
+		etherscan({
+			apiKey: process.env.ETHERSCAN_API_KEY!,
+			chainId: mainnet.id,
+			contracts: [
+				{
+					name: "WagmiMintExample",
+					address: {
+						[chains.mainnet.id]: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
+						[chains.goerli.id]: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
+					},
+				},
+			],
+		}),
+		react(),
+	],
 });
