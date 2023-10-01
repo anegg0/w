e1c34f787f97c02f7a4ff11b5aefcb50006cfbdc
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-09-30 21:06:32 -0700

    Better code for file manipulation

--- src/app/api/encodewm/route.ts
+++ src/app/api/encodewm/route.ts
@@ -8,70 +8,56 @@ import { promisify } from "util";
 
 const asyncExec = promisify(exec);
 
-// Function to calculate SHA-256 hash of a file
+export async function POST(req: NextRequest, res: NextResponse) {
+
+const signature = await req.json();
 async function calculateFileHash(filePath) {
-  const fileData = await fs.readFile(filePath);
-  const hash = crypto.createHash("sha256");
-  hash.update(fileData);
-  return hash.digest("hex");
+const fileData = await fs.readFile(filePath);
+const hash = crypto.createHash("sha256");
+hash.update(fileData);
+return hash.digest("hex");
+}
+async function createFilePath(directoryPath, fileName ) {
+let filePath;
+try {
+  filePath = path.join(directoryPath, fileName);
+  if (await fs.access(filePath).then(() => true).catch(() => false)) {
+    await fs.rm(filePath);
+  }
+  filePath = path.join(directoryPath, fileName);
+  return filePath;
+
+} catch (error) {
+  console.error('Error while clearing directory:', error);
+}
 }
 
-export async function POST(req: NextRequest, res: NextResponse) {
-  const jsonData = await req.json();
+const signatureDirectory = path.join(process.cwd(), process.env.STORE_SIGNATURE_PATH!);
+let  signatureFilePath = await createFilePath(signatureDirectory, 'signature.json');
+let signatureFile = await fs.writeFile(signatureFilePath, signature);
 
-  const signatureFileDirectory = path.join(
-    process.cwd(),
-    process.env.STORE_SIGNATURE_PATH!
-  );
+const imageFilesDirectory = path.join(process.cwd(), process.env.STORE_IMAGE_PATH!);
+const originalImage = path.join(imageFilesDirectory, 'original_image.png');
 
-  const signatureFileName = `signature.json`; // Generate a unique filename
-  const signatureFilePath = path.join(
-    signatureFileDirectory,
-    signatureFileName
-  );
+const EncodedImageFilesDirectory = path.join(process.cwd(), process.env.STORE_ENCODED_IMAGE_PATH!);
+const EncodedImageFilePath = await createFilePath( EncodedImageFilesDirectory, `encoded_image.png` );
+try {
 
-  if (existsSync(signatureFileName)) {
-    await fs.rm(signatureFileName);
-  }
+    // const encoded_image = await asyncExec(
+      // `java -jar openstego.jar --embed --algorithm=randomlsb --messagefile=${signatureFile} --coverfile=${originalImage} --stegofile=${EncodedImageFilePath}`);
 
-  const jsonFile = await fs.writeFile(
-    path.join(signatureFileDirectory, signatureFileName),
-    jsonData
-  );
-
-  const imageFilesDirectory = path.join(
-    process.cwd(),
-    process.env.STORE_IMAGE_PATH!
-  );
-
-  const originalImageFileName = `original_image.png`; // Generate a unique filename
-  const originalImageFilePath = path.join(
-    imageFilesDirectory,
-    originalImageFileName
-  );
-
-  const watermarkedImageFileName = `encoded_image.png`; // Generate a unique filename
-  const watermarkedImageFilePath = path.join(
-    imageFilesDirectory,
-    watermarkedImageFileName
-  );
-
-  if (existsSync(watermarkedImageFilePath)) {
-    unlinkSync(watermarkedImageFilePath);
-  }
-
-  try {
-    const encoded_imagePath = await asyncExec(
-      `java -jar openstego.jar --embed --algorithm=randomlsb --messagefile=${signatureFilePath} --coverfile=${originalImageFilePath} --stegofile=${watermarkedImageFilePath}`
-    );
-    console.log("Embedding successful.");
-
-    // Calculate the SHA-256 hash of the watermarked image
-    const hashedData = await calculateFileHash(watermarkedImageFilePath);
-    console.log("Hash of watermarked image:", hashedData);
-    return new NextResponse(hashedData);
-  } catch (error) {
-    console.error("Error during embedding:", error);
-    throw error;
-  }
+  const encoded_image = await asyncExec(`java -jar openstego.jar --embed --algorithm=randomlsb --messagefile=/home/o/dev/webapp/src/public/signatures/signature.json --coverfile=/home/o/dev/webapp/src/public/uploads/original_image.png --stegofile=/home/o/dev/webapp/src/public/encoded/encoded_image.png`);
+    console.log("Encoded image:", encoded_image);
+} catch (err) {
+    console.error(err);
+  // Do something
+}
+//     // Calculate the SHA-256 hash of the watermarked image
+//     const hashedData = await calculateFileHash(watermarkedImageFilePath);
+//     console.log("Hash of watermarked image:", hashedData);
+//     return new NextResponse(hashedData);
+//   } catch (error) {
+//     console.error("Error during embedding:", error);
+//     throw error;
+//   }
 }
