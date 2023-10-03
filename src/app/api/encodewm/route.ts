691e88b90f7724144c62c3cdc521eadfe18487d6
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-02 19:55:32 -0700

    uploads encoded_image.png to nft.storage

--- src/app/api/encodewm/route.ts
+++ src/app/api/encodewm/route.ts
@@ -1,74 +1,113 @@
 import fs from "fs/promises";
 import * as crypto from "crypto";
 import path from "path";
-import { existsSync, unlinkSync } from "fs";
+import { existsSync, unlinkSync, access, rm } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
 import { exec } from "child_process";
 import { promisify } from "util";
+import axios from "axios";
 
 const asyncExec = promisify(exec);
 
+async function calculateFileHash(filePath: string) {
+  const fileData = await fs.readFile(filePath);
+  return crypto.createHash("sha256").update(fileData).digest("hex");
+}
+
+async function createFilePath(directoryPath: string, fileName: string) {
+  let filePath: string;
+  try {
+    filePath = path.join(directoryPath, fileName);
+    if (
+      await access(filePath)
+        .then(() => true)
+        .catch(() => false)
+    ) {
+      await rm(filePath);
+    }
+    filePath = path.join(directoryPath, fileName);
+    return filePath;
+  } catch (error) {
+    console.error("Error while clearing directory:", error);
+  }
+}
+
+async function uploadToNftStorage(encodedImagePath: string, apiKey: string) {
+  const fileData = await fs.readFile(encodedImagePath);
+  const response = await axios.post(
+    "https://api.nft.storage/upload",
+    fileData,
+    {
+      headers: {
+        "Content-Type": "application/octet-stream",
+        Authorization: `Bearer ${apiKey}`,
+      },
+    }
+  );
+  return response.data.value.cid;
+}
+
 export async function POST(req: NextRequest, res: NextResponse) {
   const signature = await req.json();
-  async function calculateFileHash(filePath: string) {
-    const fileData = await fs.readFile(filePath);
-    const hash = crypto.createHash("sha256");
-    hash.update(fileData);
-    return hash.digest("hex");
-  }
-  async function createFilePath(directoryPath, fileName) {
-    let filePath: string;
-    try {
-      filePath = path.join(directoryPath, fileName);
-      if (
-        await fs
-          .access(filePath)
-          .then(() => true)
-          .catch(() => false)
-      ) {
-        await fs.rm(filePath);
-      }
-      filePath = path.join(directoryPath, fileName);
-      return filePath;
-    } catch (error) {
-      console.error("Error while clearing directory:", error);
-    }
-  }
 
   try {
+    const originalImageDirPath = path.join(
+      process.cwd(),
+      process.env.STORE_IMAGE_PATH!
+    );
+    const unwatermarkedFilePath = path.join(
+      originalImageDirPath,
+      "original_image.png"
+    );
     const signatureDirectory = path.join(
       process.cwd(),
       process.env.STORE_SIGNATURE_PATH!
     );
-
-    let signatureFilePath = await createFilePath(
+    const signatureFilePath = await createFilePath(
       signatureDirectory,
       "signature.json"
     );
-
-    let signatureFile = await fs.writeFile(signatureFilePath, signature);
+    await fs.writeFile(signatureFilePath, signature);
 
     const EncodedImageFilesDirectory = path.join(
       process.cwd(),
       process.env.STORE_ENCODED_IMAGE_PATH!
     );
-
-    const EncodedImageFilePath = await createFilePath(
+    const EncodedImageFilePath = path.join(
       EncodedImageFilesDirectory,
-      `encoded_image.png`
+      "encoded_image.png"
     );
     const encoded_image = await asyncExec(
-      `java -jar openstego.jar --embed --algorithm=randomlsb --messagefile=/home/o/dev/webapp/src/public/signatures/signature.json --coverfile=/home/o/dev/webapp/src/public/uploads/original_image.png --stegofile=/home/o/dev/webapp/src/public/encoded/encoded_image.png`
+      `java -jar openstego.jar --embed --a randomlsb --mf ${signatureFilePath} --cf=/home/o/dev/webapp/src/public/uploads/original_image.png --stegofile=/home/o/dev/webapp/src/public/encoded/encoded_image.png`
     );
 
     const HashesDirectory = path.join(
       process.cwd(),
       process.env.STORE_HASH_PATH!
     );
-    const HashFilePath = await createFilePath(HashesDirectory, `hash.json`);
-
+    const HashFilePath = await createFilePath(HashesDirectory, "hash.json");
     const hashedData = await calculateFileHash(EncodedImageFilePath);
-    let hashFile = await fs.writeFile(HashFilePath, hashedData);
+    await fs.writeFile(HashFilePath, hashedData);
+
+    const apiKey = process.env.NFT_STORAGE_API_KEY;
+    const cid = await uploadToNftStorage(EncodedImageFilePath, apiKey);
+
+    const metadataFileDirectory = path.join(
+      process.cwd(),
+      process.env.STORE_METADATA_FILE!
+    );
+
+    const metadataFilePath = await createFilePath(
+      metadataFileDirectory,
+      `metadata.json`
+    );
+    const metadata = {
+      description: "Signed NFT",
+      external_url: `ipfs://${cid}`,
+      name: "Dave Starbelly",
+    };
+    await fs.writeFile(metadataFilePath, JSON.stringify(metadata, null, 2));
+
     return new NextResponse(hashedData);
   } catch (error) {
     console.error("Error returned at end of executions:", error);
