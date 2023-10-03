8accf7857423d58cb991a1e38dcb977fc2582ae3
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-03 16:53:11 -0700

    background execution of encoded file upload

--- src/app/api/encodewm/route.ts
+++ src/app/api/encodewm/route.ts
@@ -50,69 +50,73 @@ async function uploadToNftStorage(encodedImagePath: string, apiKey: string) {
 export async function POST(req: NextRequest, res: NextResponse) {
   const signature = await req.json();
 
-  try {
-    const originalImageDirPath = path.join(
-      process.cwd(),
-      process.env.STORE_IMAGE_PATH!
-    );
-    const unwatermarkedFilePath = path.join(
-      originalImageDirPath,
-      "original_image.png"
-    );
-    const signatureDirectory = path.join(
-      process.cwd(),
-      process.env.STORE_SIGNATURE_PATH!
-    );
-    const signatureFilePath = await createFilePath(
-      signatureDirectory,
-      "signature.json"
-    );
-    await fs.writeFile(signatureFilePath, signature);
+  (async () => {
+    try {
+      const originalImageDirPath = await path.join(
+        process.cwd(),
+        process.env.STORE_IMAGE_PATH!
+      );
+      const unwatermarkedFilePath = await path.join(
+        originalImageDirPath,
+        "original_image.png"
+      );
+      const signatureDirectory = await path.join(
+        process.cwd(),
+        process.env.STORE_SIGNATURE_PATH!
+      );
+      const signatureFilePath = await createFilePath(
+        signatureDirectory,
+        "signature.json"
+      );
+      await fs.writeFile(signatureFilePath, signature);
 
-    const EncodedImageFilesDirectory = path.join(
-      process.cwd(),
-      process.env.STORE_ENCODED_IMAGE_PATH!
-    );
-    const EncodedImageFilePath = path.join(
-      EncodedImageFilesDirectory,
-      "encoded_image.png"
-    );
-    const encoded_image = await asyncExec(
-      `java -jar openstego.jar --embed --a randomlsb --mf ${signatureFilePath} --cf=/home/o/dev/webapp/src/public/uploads/original_image.png --stegofile=/home/o/dev/webapp/src/public/encoded/encoded_image.png`
-    );
+      const EncodedImageFilesDirectory = await path.join(
+        process.cwd(),
+        process.env.STORE_ENCODED_IMAGE_PATH!
+      );
+      const EncodedImageFilePath = await path.join(
+        EncodedImageFilesDirectory,
+        "encoded_image.png"
+      );
+      const encoded_image = await asyncExec(
+        `java -jar openstego.jar --embed --a randomlsb --mf ${signatureFilePath} --cf=/home/o/dev/webapp/src/public/uploads/original_image.png --stegofile=/home/o/dev/webapp/src/public/encoded/encoded_image.png`
+      );
 
-    const HashesDirectory = path.join(
-      process.cwd(),
-      process.env.STORE_HASH_PATH!
-    );
-    const HashFilePath = await createFilePath(HashesDirectory, "hash.json");
-    const hashedData = await calculateFileHash(EncodedImageFilePath);
-    await fs.writeFile(HashFilePath, hashedData);
+      const HashesDirectory = await path.join(
+        process.cwd(),
+        process.env.STORE_HASH_PATH!
+      );
+      const HashFilePath = await createFilePath(HashesDirectory, "hash.json");
+      const hashedData = await calculateFileHash(EncodedImageFilePath);
+      await fs.writeFile(HashFilePath, hashedData);
 
-    const apiKey = process.env.NFT_STORAGE_API_KEY;
-    const cid = await uploadToNftStorage(EncodedImageFilePath, apiKey);
+      const apiKey = await process.env.NFT_STORAGE_API_KEY;
+      const cid = await uploadToNftStorage(EncodedImageFilePath, apiKey);
 
-    const metadataFileDirectory = path.join(
-      process.cwd(),
-      process.env.STORE_METADATA_FILE!
-    );
+      const metadataFileDirectory = await path.join(
+        process.cwd(),
+        process.env.STORE_METADATA_FILE!
+      );
 
-    const metadataFilePath = await createFilePath(
-      metadataFileDirectory,
-      `metadata.json`
-    );
-    const metadata = {
-      description: "Signed NFT",
-      external_url: `ipfs://${cid}`,
-      name: "Dave Starbelly",
-    };
-    const metadataFileContent = await JSON.stringify(metadata, null, 2);
-    console.log(`metadata.json is: ${metadataFileContent}`);
+      const metadataFilePath = await createFilePath(
+        metadataFileDirectory,
+        `metadata.json`
+      );
+      const metadata = {
+        description: "Signed NFT",
+        external_url: `ipfs://${cid}`,
+        name: "Dave Starbelly",
+      };
+      const metadataFileContent = await JSON.stringify(metadata, null, 2);
+      console.log(`metadata.json is: ${metadataFileContent}`);
 
-    await fs.writeFile(metadataFilePath, metadataFileContent);
-    return new NextResponse(metadataFileContent);
-  } catch (error) {
-    console.error("Error returned at end of executions:", error);
-    throw error;
-  }
+      await fs.writeFile(metadataFilePath, metadataFileContent);
+
+      console.log("Background execution completed");
+    } catch (error) {
+      console.error("Error in background execution:", error);
+    }
+  })();
+
+  return new NextResponse(null, { status: 200 });
 }
