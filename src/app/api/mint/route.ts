b0bd3eb28122545218a1f7d51d91346eec6ebe9a
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-06 02:14:19 -0700

    added package prettier to the project

--- src/app/api/mint/route.ts
+++ src/app/api/mint/route.ts
@@ -4,45 +4,45 @@ import path from "path";
 import { NFTStorage, File } from "nft.storage";
 import mime from "mime";
 export async function POST(req: NextRequest, res: NextResponse) {
-  try {
-    const { name, description } = await req.json();
-    async function fileFromPath(filePath: string) {
-      const content = await fs.readFile(filePath);
-      const type = mime.getType(filePath);
-      return new File([content], path.basename(filePath), { type });
-    }
-    const imagePath = path.join(
-      process.cwd(),
-      "src/app/encoded/encoded_image.png"
-    );
-    const NFT_STORAGE_TOKEN = process.env.NFT_STORAGE_API_KEY;
-    async function storeNFT(
-      imagePath: string,
-      name: string,
-      description: string
-    ) {
-      const image = await fileFromPath(imagePath);
-      const nftstorage = new NFTStorage({ token: NFT_STORAGE_TOKEN });
-      return nftstorage.store({
-        image,
-        name,
-        description,
-      });
-    }
-    const nftResult = await storeNFT(imagePath, name, description);
-    console.log(`Metadata updated successfully! ${nftResult.url}`);
-    return NextResponse.json({
-      success: true,
-      message: "Metadata updated successfully!",
-    });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      {
-        success: false,
-        message: "Internal Server Error",
-      },
-      { status: 500 }
-    );
-  }
+	try {
+		const { name, description } = await req.json();
+		async function fileFromPath(filePath: string) {
+			const content = await fs.readFile(filePath);
+			const type = mime.getType(filePath);
+			return new File([content], path.basename(filePath), { type });
+		}
+		const imagePath = path.join(
+			process.cwd(),
+			"src/app/encoded/encoded_image.png",
+		);
+		const NFT_STORAGE_TOKEN = process.env.NFT_STORAGE_API_KEY;
+		async function storeNFT(
+			imagePath: string,
+			name: string,
+			description: string,
+		) {
+			const image = await fileFromPath(imagePath);
+			const nftstorage = new NFTStorage({ token: NFT_STORAGE_TOKEN });
+			return nftstorage.store({
+				image,
+				name,
+				description,
+			});
+		}
+		const nftResult = await storeNFT(imagePath, name, description);
+		console.log(`Metadata updated successfully! ${nftResult.url}`);
+		return NextResponse.json({
+			success: true,
+			message: "Metadata updated successfully!",
+		});
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json(
+			{
+				success: false,
+				message: "Internal Server Error",
+			},
+			{ status: 500 },
+		);
+	}
 }
--- src/components/MintNFT.tsx
+++ src/components/MintNFT.tsx
@@ -1,15 +1,15 @@
-'use client'
-
-import { useState } from 'react'
-import { useWaitForTransaction } from 'wagmi'
+"use client";
+import { useState, useEffect } from "react";
+import { useWaitForTransaction } from "wagmi";
 
 import {
   usePrepareWagmiMintExampleMint,
   useWagmiMintExampleMint,
-} from '../generated'
+} from "../generated";
 
 export function MintNFT() {
-  const [tokenId, setTokenId] = useState('')
+  const [tokenId, setTokenId] = useState("");
+  const [nftUrl, setNftUrl] = useState("");
 
   const {
     config,
@@ -17,12 +17,19 @@ export function MintNFT() {
     isError: isPrepareError,
   } = usePrepareWagmiMintExampleMint({
     args: tokenId ? [BigInt(tokenId)] : undefined,
-  })
-  const { data, error, isError, write } = useWagmiMintExampleMint(config)
+  });
+  const { data, error, isError, write } = useWagmiMintExampleMint(config);
+
+  useEffect(() => {
+    if (data && data.result) {
+      const nftResult = data.result;
+      setNftUrl(nftResult.url);
+    }
+  }, [data]);
 
   const { isLoading, isSuccess } = useWaitForTransaction({
     hash: data?.hash,
-  })
+  });
 
   return (
     <div>
@@ -32,7 +39,7 @@ export function MintNFT() {
         value={tokenId}
       />
       <button disabled={!write || isLoading} onClick={() => write?.()}>
-        {isLoading ? 'Minting...' : 'Mint'}
+        {isLoading ? "Minting..." : "Mint"}
       </button>
       {isSuccess && (
         <div>
@@ -40,11 +47,17 @@ export function MintNFT() {
           <div>
             <a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
           </div>
+          <ComponentWithNftUrl nftUrl={nftUrl} />
         </div>
       )}
       {(isPrepareError || isError) && (
         <div>Error: {(prepareError || error)?.message}</div>
       )}
     </div>
-  )
+  );
+}
+
+function ComponentWithNftUrl({ nftUrl }) {
+  // Component with #66 can access nftUrl here and use it as needed
+  return <div>NFT URL: {nftUrl}</div>;
 }
