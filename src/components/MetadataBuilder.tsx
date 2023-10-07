b9697aafb61472195d74d0590775202a85374c58
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-07 03:25:41 -0700

    Partly functional, still routes to mint component

--- src/components/MetadataBuilder.tsx
+++ src/components/MetadataBuilder.tsx
@@ -1,10 +1,57 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
 
 interface FormData {
   name: string;
   description: string;
+}
+
+function MintNFTForm({ tokenURI }) {
+  const [tokenId, setTokenId] = React.useState("");
+  const debouncedTokenId = useDebounce(tokenId);
+
+  const {
+    config,
+    error: prepareError,
+    isError: isPrepareError,
+  } = usePrepareContractWrite({
+    address: "0xEf40F59aeA57997D5A4CEb7af089baCba10d01CA",
+    abi: [
+      {
+        name: "mintNFT",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [{ internalType: "string", name: "tokenURI", type: "string" }],
+        outputs: [],
+      },
+    ],
+    functionName: "mintNFT",
+    args: tokenURI,
+  });
+  const { data, error, isError, write } = useContractWrite(config);
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+  return (
+    <div>
+      {isSuccess && (
+        <div>
+          Successfully minted your NFT!
+          <div>
+            <a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export function MetadataBuilder({ onSuccessfulMetadataCreation }) {
@@ -34,6 +81,7 @@ export function MetadataBuilder({ onSuccessfulMetadataCreation }) {
       });
       console.log("Response:", response);
       if (response.ok) {
+        /* MintNFTForm({ tokenURI }); */
         onSuccessfulMetadataCreation(4);
       } else {
         const data = await response.json();
