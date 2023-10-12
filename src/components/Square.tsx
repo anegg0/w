ee9e99d56bbed6da6ea4a769503fcaf07978b78e
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-11 18:03:25 -0700

    improved Square's background color

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export function Square() {
   return (
     <div
-      className="rounded-md mx-64 my-6 p-3 h-2/3  flex items-center justify-center"
-      style={{ background: "#3d3846" }}
+      className=" z-0 rounded-md mx-64 my-6 p-3 h-2/3  flex items-center justify-center"
+      style={{ background: "#1D1F21" }}
     ></div>
   );
 }
