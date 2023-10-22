381e2bde7474475d87008d4523fba2e2ee232371
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-21 19:55:41 -0700

    Balanced footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,34 @@ export function Footer() {
   return (
     <footer id="subscribe" aria-label="Site Footer">
       <div className="max-w-screen-xl px-2 py-20 mx-auto sm:px-6 lg:px-8">
-        <div className="lg:flex lg:items-start lg:gap-8">
+        <div className="lg:flex lg:items-center lg:gap-8">
           <div className="text-teal-600">
             <Image src={logo} width={52} height={52} alt="W Logo" />
           </div>
+          <ul className="flex flex-grow justify-end col-span-2 gap-6 lg:col-span-5">
+            <li>
+              <a
+                href="https://github.com/anegg0/W"
+                rel="noreferrer"
+                target="_blank"
+                className="text-white transition hover:opacity-75"
+              >
+                <span className="sr-only">GitHub</span>
+                <svg
+                  className="w-9 h-9"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </li>
+          </ul>
           <div className="grid grid-cols-2 gap-8 mt-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16">
             <div className="col-span-2">
               <div>
@@ -17,30 +41,6 @@ export function Footer() {
                 <p className="mt-4 text-white"></p>
               </div>
             </div>
-            <ul className="flex justify-start col-span-2 gap-6 lg:col-span-5 lg:justify-end">
-              <li>
-                <a
-                  href="https://github.com/anegg0/w"
-                  rel="noreferrer"
-                  target="_blank"
-                  className="text-white transition hover:opacity-75"
-                >
-                  <span className="sr-only">GitHub</span>
-                  <svg
-                    className="w-6 h-6"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
         <div className="pt-8 mt-8 border-t border-gray-100">
