d1c6a5e8eeee3d87fdd448dc738daea306390583
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-21 19:07:20 -0700

    Dark theme for rainbowkit

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 import * as React from "react";
 import { WagmiConfig } from "wagmi";
 
@@ -10,7 +10,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
   React.useEffect(() => setMounted(true), []);
   return (
     <WagmiConfig config={config}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider
+        theme={darkTheme({ borderRadius: "small" })}
+        chains={chains}
+      >
         {mounted && children}
       </RainbowKitProvider>
     </WagmiConfig>
