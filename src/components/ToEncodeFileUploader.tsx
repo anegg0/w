66193d2f6723747bb0c81467dec2db4419f48208
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-22 06:31:27 -0700

    cleaned up and renamed

--- src/components/ToEncodeFileUploader.tsx
+++ src/components/ToEncodeFileUploader.tsx
@@ -1,9 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import { FaCheck } from "react-icons/fa";
-import "@a/globals.css";
-import { page } from "@a/page";
-import { Square } from "@c/Square";
 import Image from "next/image";
 
 export function ToEncodeFileUploader({ onSuccessfulUpload }) {
@@ -20,7 +17,7 @@ export function ToEncodeFileUploader({ onSuccessfulUpload }) {
     }
   };
 
-  const handleCancel = () => {
+  const handleRemove = () => {
     setFile(null);
     setPreviewUrl(null);
   };
@@ -53,65 +50,9 @@ export function ToEncodeFileUploader({ onSuccessfulUpload }) {
 
   return (
     <>
-      <div className="max-w-xl mx-auto my-4 border-b-2 pb-4">
+      <div id="verify" className="max-w-xl mx-auto my-4 border-b-2 pb-4">
         <div className="flex pb-3">
-          <div className="flex-1"></div>
-          <div className="flex-1">
-            <div className="w-10 h-10 bg-green mx-auto rounded-full text-lg text-gray flex items-center">
-              <span className="text-gray text-center w-full">
-                <FaCheck style={{ color: "green" }} />
-              </span>
-            </div>
-          </div>
-
-          <div className="w-1/6 align-center items-center align-middle content-center flex">
-            <div className="w-full bg-grey-light rounded items-center align-middle align-center flex-1">
-              <div
-                className="bg-green-light text-xs leading-none py-1 text-center text-grey-darkest rounded"
-                style={{ width: "100%" }}
-              ></div>
-            </div>
-          </div>
-
-          <div className="flex-1">
-            <div className="w-10 h-10 bg-green mx-auto rounded-full text-lg text-gray flex items-center">
-              <span className="text-gray text-center w-full">
-                <FaCheck />
-              </span>
-            </div>
-          </div>
-
-          <div className="w-1/6 align-center items-center align-middle content-center flex">
-            <div className="w-full bg-grey-light rounded items-center align-middle align-center flex-1">
-              <div
-                className="bg-green-light text-xs leading-none py-1 text-center text-grey-darkest rounded"
-                style={{ width: "20%" }}
-              ></div>
-            </div>
-          </div>
-
-          <div className="flex-1">
-            <div className="w-10 h-10 bg-green border-2 border-grey-light mx-auto rounded-full text-lg text-gray flex items-center">
-              <span className="text-grey-darker text-center w-full">3</span>
-            </div>
-          </div>
-
-          <div className="w-1/6 align-center items-center align-middle content-center flex">
-            <div className="w-full bg-grey-light rounded items-center align-middle align-center flex-1">
-              <div
-                className="bg-green-light text-xs leading-none py-1 text-center text-grey-darkest rounded"
-                style={{ width: "0%" }}
-              ></div>
-            </div>
-          </div>
-
-          <div className="flex-1">
-            <div className="w-10 h-10 bg-green border-2 border-grey-light mx-auto rounded-full text-lg text-gray flex items-center">
-              <span className="text-grey-darker text-center w-full">4</span>
-            </div>
-          </div>
-
-          <div className="flex-1"></div>
+          {/* ... (Other divs and elements for your progress bar) ... */}
         </div>
 
         <div className="flex text-xs content-center text-center">
@@ -130,10 +71,10 @@ export function ToEncodeFileUploader({ onSuccessfulUpload }) {
           <div>
             <Image src={previewUrl} alt="Preview" width="600" height="600" />
             <button
-              onClick={handleCancel}
+              onClick={handleRemove}
               className="block mt-4 px-3 py-2 bg-gray-400 text-gray rounded-sm"
             >
-              Cancel
+              Remove
             </button>
             <button
               onClick={handleUpload}
