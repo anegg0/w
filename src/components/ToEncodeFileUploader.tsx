29db7b88b222b24260ac428ac9f729e65b7a136a
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-22 21:43:13 -0700

    Re-added checks on task progress

--- src/components/ToEncodeFileUploader.tsx
+++ src/components/ToEncodeFileUploader.tsx
@@ -51,8 +51,66 @@ export function ToEncodeFileUploader({ onSuccessfulUpload }) {
   return (
     <>
       <div id="verify" className="max-w-xl mx-auto my-4 border-b-2 pb-4">
-        <div className="flex pb-3">
-          {/* ... (Other divs and elements for your progress bar) ... */}
+        <div className="max-w-xl mx-auto my-4 border-b-2 pb-4">
+          <div className="flex pb-3">
+            <div className="flex-1"></div>
+            <div className="flex-1">
+              <div className="w-10 h-10 bg-green mx-auto rounded-full text-lg text-gray flex items-center">
+                <span className="text-gray text-center w-full">
+                  <FaCheck style={{ color: "green" }} />
+                </span>
+              </div>
+            </div>
+
+            <div className="w-1/6 align-center items-center align-middle content-center flex">
+              <div className="w-full bg-grey-light rounded items-center align-middle align-center flex-1">
+                <div
+                  className="bg-green-light text-xs leading-none py-1 text-center text-grey-darkest rounded"
+                  style={{ width: "100%" }}
+                ></div>
+              </div>
+            </div>
+
+            <div className="flex-1">
+              <div className="w-10 h-10 bg-green mx-auto rounded-full text-lg text-gray flex items-center">
+                <span className="text-gray text-center w-full">
+                  <FaCheck />
+                </span>
+              </div>
+            </div>
+
+            <div className="w-1/6 align-center items-center align-middle content-center flex">
+              <div className="w-full bg-grey-light rounded items-center align-middle align-center flex-1">
+                <div
+                  className="bg-green-light text-xs leading-none py-1 text-center text-grey-darkest rounded"
+                  style={{ width: "20%" }}
+                ></div>
+              </div>
+            </div>
+
+            <div className="flex-1">
+              <div className="w-10 h-10 bg-green border-2 border-grey-light mx-auto rounded-full text-lg text-gray flex items-center">
+                <span className="text-grey-darker text-center w-full">3</span>
+              </div>
+            </div>
+
+            <div className="w-1/6 align-center items-center align-middle content-center flex">
+              <div className="w-full bg-grey-light rounded items-center align-middle align-center flex-1">
+                <div
+                  className="bg-green-light text-xs leading-none py-1 text-center text-grey-darkest rounded"
+                  style={{ width: "0%" }}
+                ></div>
+              </div>
+            </div>
+
+            <div className="flex-1">
+              <div className="w-10 h-10 bg-green border-2 border-grey-light mx-auto rounded-full text-lg text-gray flex items-center">
+                <span className="text-grey-darker text-center w-full">4</span>
+              </div>
+            </div>
+
+            <div className="flex-1"></div>
+          </div>
         </div>
 
         <div className="flex text-xs content-center text-center">
