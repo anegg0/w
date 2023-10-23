d05b44a3c2214f10736251297afc421a2dee03db
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-23 00:26:45 -0700

    aligned StepProgressBar's elements

--- src/components/StepProgressBar.tsx
+++ src/components/StepProgressBar.tsx
@@ -3,55 +3,24 @@ import { FaCheck } from "react-icons/fa";
 
 const StepProgressBar = ({ steps, currentStep, id }) => {
   return (
-    <div id={id} className="max-w-xl mx-auto my-4 border-b-2 pb-4">
-      <div className="flex pb-3">
-        <div className="flex-1"></div>
+    <div id={id} className="max-w-xl mx-auto my-2 border-b-2 pb-2 w-full">
+      <div className="flex w-full justify-between">
         {steps.map((step, index) => (
-          <React.Fragment key={index}>
-            <div className="flex-1">
-              <div
-                className={`w-10 h-10 mx-auto rounded-full text-lg text-gray flex items-center ${
-                  currentStep > index
-                    ? "bg-green"
-                    : "border-2 border-grey-light"
-                }`}
-              >
-                <span className="text-gray text-center w-full">
-                  {currentStep > index ? (
-                    <FaCheck style={{ color: "green" }} />
-                  ) : (
-                    index + 1
-                  )}
-                </span>
-              </div>
+          <div key={index} className="flex flex-col items-center w-1/4">
+            <div
+              className={`w-10 h-10 rounded-full text-lg text-gray flex items-center justify-center ${
+                currentStep > index ? "bg-green" : "border-2 border-grey-light"
+              }`}
+            >
+              <span className="text-center">
+                {currentStep > index ? (
+                  <FaCheck style={{ color: "green" }} />
+                ) : (
+                  index + 1
+                )}
+              </span>
             </div>
-            {index !== steps.length - 1 && (
-              <div className="w-1/6 align-center items-center align-middle content-center flex">
-                <div className="w-full bg-grey-light rounded items-center align-middle align-center flex-1">
-                  <div
-                    className="bg-green-light text-xs leading-none py-1 text-center text-grey-darkest rounded"
-                    style={{
-                      width: `${
-                        currentStep > index + 1
-                          ? 100
-                          : currentStep === index + 1
-                          ? 20
-                          : 0
-                      }%`,
-                    }}
-                  ></div>
-                </div>
-              </div>
-            )}
-          </React.Fragment>
-        ))}
-        <div className="flex-1"></div>
-      </div>
-
-      <div className="flex text-xs content-center text-center">
-        {steps.map((label, index) => (
-          <div key={index} className="w-1/4">
-            {label}
+            <div className="text-xs mt-0.5">{step}</div>
           </div>
         ))}
       </div>
