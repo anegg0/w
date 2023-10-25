36740711f730a43ffbd40173b6f30859b5effb11
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-24 23:41:33 -0700

    Removed unusued classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,65 +19,33 @@ function ConnectWallet({ show = "always" }) {
 
 export function Header() {
   return (
-    <header aria-label="Site Header">
-      <div className="m-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center md:m-8 justify-between">
-          <div className="md:flex max-w-sm md:items-center md:gap-12">
-            <Link href="/" className="block text-teal-600">
-              <span className="sr-only">Home</span>
-              <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-                <Image src={logo} width={52} height={52} alt="W Logo" />
-              </div>
+    <div className="header">
+      <Link href="/" className="block text-teal-600">
+        <span className="sr-only">Home</span>
+        <Image src={logo} width={52} height={52} alt="W Logo" />
+      </Link>
+      <nav aria-label="Site Nav">
+        <ul className="flex justify-between gap-16 text-md">
+          <li>
+            <Link
+              href="/#about"
+              className="text-gray-300 transition hover:text-gray-300/75"
+            >
+              How Does this Thing Work?
             </Link>
-          </div>
-          <div className="hidden md:block">
-            <nav aria-label="Site Nav">
-              <ul className="flex justify-between gap-6 text-md">
-                <li>
-                  <Link
-                    href="/#about"
-                    className="text-gray-300 transition hover:text-gray-300/75"
-                  >
-                    How Does this Thing Work?
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/verify"
-                    className="text-gray-300 transition hover:text-gray-300/75"
-                  >
-                    Verify a Picture
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className="flex items-center gap-4">
-            <div className="sm:flex sm:gap-4">
-              <ConnectButton></ConnectButton>
-            </div>
-          </div>
-          <div className="block md:hidden">
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-    </header>
+          </li>
+          <li>
+            <Link
+              href="/verify"
+              className="text-gray-300 transition hover:text-gray-300/75"
+            >
+              Verify a Picture
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <ConnectButton></ConnectButton>
+    </div>
   );
 }
 export default Header;
