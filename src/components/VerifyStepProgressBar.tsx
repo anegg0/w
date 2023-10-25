606da33c29ffb429d48977b397356060b2bc4362
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-10-24 19:56:42 -0700

    Added StepProgress component designed for verification process

--- src/components/VerifyStepProgressBar.tsx
+++ src/components/VerifyStepProgressBar.tsx
@@ -6,7 +6,7 @@ const VerifyStepProgressBar = ({ steps, currentStep, id }) => {
     <div id={id} className="max-w-xl mx-auto my-2 border-b-2 pb-2 w-full">
       <div className="flex w-full justify-between">
         {steps.map((step, index) => (
-          <div key={index} className="flex flex-col items-center w-1/4">
+          <div key={index} className="flex flex-col items-center w-1/2">
             <div
               className={`w-10 h-10 rounded-full text-lg text-gray flex items-center justify-center ${
                 currentStep > index ? "bg-green" : "border-2 border-grey-light"
