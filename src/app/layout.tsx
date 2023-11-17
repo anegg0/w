24f1dc7594502d7b64756d9ed3d422fbb8593e37
Gael Blanchemain <gael.blanchemain@protonmail.com>
2023-11-17 13:54:32 -0800

    Added metada + manifest for next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import Head from "next/head";
 import { Providers } from "@a/providers";
 import "@rainbow-me/rainbowkit/styles.css";
+
+export const metadata: Metadata = {
+  title: "w",
+  description: "Steganography + Crypto + NFTs",
+  /* manifest: "@p/manifest.json", */
+  manifest: "./public/manifest.json",
+  themeColor: "#000000",
+};
 export default function RootLayout({
   children,
 }: {
